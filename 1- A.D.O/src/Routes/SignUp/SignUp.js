25331fccbd3e26ab166fb81374aa936c7c374b82
joaohenr <joaohenriqueferreiragoncalves@gmail.com>
2025-04-01 15:44:53 -0300

    Tela de Cadastro criada e Biblioteca de Ícones implementada

--- 1- A.D.O/src/Routes/SignUp/SignUp.js
+++ 1- A.D.O/src/Routes/SignUp/SignUp.js
@@ -1,9 +1,63 @@
 import React from "react";
+import "./styles.css"
+import { BsEnvelope } from "react-icons/bs"
 
-export default function SignUp(){
+export default function SignUp() {
     return (
-        <div>
-            <p>Tela de Cadastro</p>
+        <div className="main-sign">
+            <h1 className="title-sign">Cadastre-se</h1>
+
+            <form className="container-form-sign">
+
+                <div className="container-input-sign">
+                    <span className="title-input-sign">Congregação</span>
+                    <input placeholder="Nome da Congregação" />
+                </div>
+
+                <div className="container-input-sign">
+                    <span className="title-input-sign">Cidade</span>
+                    <input placeholder="Digite sua Cidade" />
+                </div>
+
+                <div className="container-input-sign">
+                    <span className="title-input-sign">Estado</span>
+                    <input placeholder="Ex: RJ" />
+                </div>
+
+                <div className="container-input-sign">
+                    <span className="title-input-sign">Responsável</span>
+                    <input placeholder="Nome do Responsável" />
+                </div>
+
+                <div className="container-input-sign">
+                    <span className="title-input-sign">Telefone</span>
+                    <input placeholder="Ex: +55XXxxxxxxxxx" />
+                </div>
+
+                <div className="container-input-sign">
+                    <span className="title-input-sign">CPF</span>
+                    <input placeholder="Ex: 68754893578" />
+                </div>
+
+                <div className="container-input-sign">
+                    <span className="title-input-sign">Senha</span>
+                    <input placeholder="Insira a senha de 4 dígitos" />
+                </div>
+
+                <button className="button-confirm-sign">Cadastrar</button>
+
+                <span className="has-account-label">Já possui uma conta? Faça login</span>
+
+                <div className="container-help-sign">
+                    <div style={{ display: 'flex', alignItems: 'center', gap: 6 }}>
+                        <span className="separator-sign"></span>
+                        <span className="help-label-sign">Em caso de dúvidas, entre em contato abaixo</span>
+                        <span className="separator-sign"></span>
+                    </div>
+                    <BsEnvelope size={30} color="rgb(182, 182, 182)" className="icon-email-sign" />
+                </div>
+
+            </form>
         </div>
     )
 }
